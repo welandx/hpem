793eeb8b42ab35a36796af6e76a18b4eec0fb5f9
weland <weland@bupt.edu.cn>
2024-02-06 23:32:53 +0800

    gitignore

---
+++ early-init.el
@@ -0,0 +1,6 @@
+(setq frame-resize-pixelwise t)
+(setq default-frame-alist '((menu-bar-lines . 0)
+                             (tool-bar-lines . 0)
+                             (alpha-background . 80)
+                             (width . 300)
+                             (height . 80)))
